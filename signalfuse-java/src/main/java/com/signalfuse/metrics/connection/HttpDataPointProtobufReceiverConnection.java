2be427078d8b7acbb51f5197b1f759578cca574d
Jack <jack@signalfuse.com>
2014-08-25 15:19:54 -0700

    Use 0.0.7-SNAPSHOT version

--- signalfuse-java/src/main/java/com/signalfuse/metrics/connection/HttpDataPointProtobufReceiverConnection.java
+++ signalfuse-java/src/main/java/com/signalfuse/metrics/connection/HttpDataPointProtobufReceiverConnection.java
@@ -35,7 +35,7 @@ public class HttpDataPointProtobufReceiverConnection implements DataPointReceive
     private static final ContentType PROTO_TYPE = ContentType.create("application/x-protobuf");
     private static final ContentType JSON_TYPE = ContentType.APPLICATION_JSON;
     // Do not modify this line.  It is auto replaced to a version number.
-    public static final String VERSION_NUMBER = "0.0.6";
+    public static final String VERSION_NUMBER = "0.0.7-SNAPSHOT";
     static final String USER_AGENT = "SignalFx-java-client/" + VERSION_NUMBER;
     private static final Logger log = LoggerFactory
             .getLogger(HttpDataPointProtobufReceiverConnection.class);
