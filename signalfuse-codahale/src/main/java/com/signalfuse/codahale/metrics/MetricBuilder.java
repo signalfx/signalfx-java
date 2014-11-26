e5da29020b73a6449e15927b8d68a4ff049a079e
Jack Lindamood <jack@signalfuse.com>
2014-11-25 16:42:38 -0800

    Expose builders for basic metric types
    Summary: builders for when metrics already exist
    
    Reviewers: max

--- signalfuse-codahale/src/main/java/com/signalfuse/codahale/metrics/MetricBuilder.java
+++ signalfuse-codahale/src/main/java/com/signalfuse/codahale/metrics/MetricBuilder.java
@@ -1,6 +1,11 @@
 package com.signalfuse.codahale.metrics;
 
+import com.codahale.metrics.Counter;
+import com.codahale.metrics.ExponentiallyDecayingReservoir;
+import com.codahale.metrics.Histogram;
+import com.codahale.metrics.Meter;
 import com.codahale.metrics.Metric;
+import com.codahale.metrics.Timer;
 
 /**
  * A copy of the MetricBuilder defined inside {@link com.codahale.metrics.MetricRegistry}, but a
@@ -11,4 +16,52 @@ public interface MetricBuilder<T extends Metric> {
     public T newMetric();
 
     public boolean isInstance(Metric metric);
+
+    public MetricBuilder<Counter> COUNTERS = new MetricBuilder<Counter>() {
+        @Override
+        public Counter newMetric() {
+            return new Counter();
+        }
+
+        @Override
+        public boolean isInstance(Metric metric) {
+            return Counter.class.isInstance(metric);
+        }
+    };
+
+    public MetricBuilder<Histogram> HISTOGRAMS = new MetricBuilder<Histogram>() {
+        @Override
+        public Histogram newMetric() {
+            return new Histogram(new ExponentiallyDecayingReservoir());
+        }
+
+        @Override
+        public boolean isInstance(Metric metric) {
+            return Histogram.class.isInstance(metric);
+        }
+    };
+
+    public MetricBuilder<Meter> METERS = new MetricBuilder<Meter>() {
+        @Override
+        public Meter newMetric() {
+            return new Meter();
+        }
+
+        @Override
+        public boolean isInstance(Metric metric) {
+            return Meter.class.isInstance(metric);
+        }
+    };
+
+    public MetricBuilder<Timer> TIMERS = new MetricBuilder<Timer>() {
+        @Override
+        public Timer newMetric() {
+            return new Timer();
+        }
+
+        @Override
+        public boolean isInstance(Metric metric) {
+            return Timer.class.isInstance(metric);
+        }
+    };
 }
