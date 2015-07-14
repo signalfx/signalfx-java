fe8762354d840865f955e19abffe4ad79e574c5d
Jack Lindamood <jack@signalfx.com>
2015-07-14 13:59:11 -0700

    Update .travis.yml to work around NPE in travis

--- update_version.py
+++ update_version.py
@@ -19,7 +19,7 @@ def execute(cmd, expected_code=None, stdin=None, background=False):
     logger.debug("Result (%s, %s, %d)", stdout, stderr, proc.returncode)
     ret = (stdout, stderr, proc.returncode)
     if expected_code is not None and expected_code != ret[2]:
-        raise ExecuteError("Unable to execute command %s, result %s/%s/%d", ret[0], ret[1], ret[2])
+        raise Exception("Unable to execute command %s, result %s/%s/%d", ret[0], ret[1], ret[2])
     return ret
 
 (stdout, _, code) = execute(["find", os.getcwd(), '-name', 'pom.xml'], expected_code=0)
