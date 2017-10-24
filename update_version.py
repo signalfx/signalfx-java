ad7b24b860fdc240c9ca1b93c5e21893375eca37
Maxime Petazzoni <maxime.petazzoni@bulix.org>
2017-10-24 09:08:09 -0700

    Improve update_version.py script to update the README as well

--- update_version.py
+++ update_version.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2017 SignalFx, Inc. All rights reserved.
+
+# This script is used to update the versions of all the artifacts, the
+# User-Agent version of the library, and the documented version in the README
+# file, all at once, as part of the release process.
+
 import logging
 import os
 import re
@@ -8,45 +16,65 @@ import sys
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-version = sys.argv[1]
+FILE_REPLACES = {
+    'signalfx-java/src/main/java/com/signalfx/connection/AbstractHttpReceiverConnection.java': [
+        (re.compile(r'public static final String VERSION_NUMBER = "(.*?)"'),
+         'public static final String VERSION_NUMBER = "%s"')
+    ],
+    'README.md': [
+        (re.compile(r'<version>([^<]+)</version>'),
+         '<version>%s</version>'),
+        (re.compile(r'libraryDependencies \+= "com.signalfx.public" % "signalfx-codahale" % "(.*?)"'),
+         'libraryDependencies += "com.signalfx.public" %% "signalfx-codahale" %% "%s"'),
+        (re.compile(r'libraryDependencies \+= "com.signalfx.public" % "signalfx-yammer" % "(.*?)"'),
+         'libraryDependencies += "com.signalfx.public" %% "signalfx-yammer" %% "%s"'),
+    ],
+}
 
 
 def execute(cmd, expected_code=None, stdin=None, background=False):
-    logger.debug("Executing %s", cmd)
+    logger.debug('Executing %s', cmd)
     proc = subprocess.Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE)
     if background:
         return ('', '', 0)  # In background
     stdout, stderr = proc.communicate(stdin)
-    logger.debug("Result (%s, %s, %d)", stdout, stderr, proc.returncode)
+    logger.debug('Result (%s, %s, %d)', stdout, stderr, proc.returncode)
     ret = (stdout, stderr, proc.returncode)
     if expected_code is not None and expected_code != ret[2]:
-        raise Exception("Unable to execute command %s, result %s/%s/%d", ret[0], ret[1], ret[2])
+        raise Exception('Unable to execute command %s, result: %s', ret)
     return ret
 
 
-def update_pom_files():
-    (stdout, _, code) = execute(["mvn", "versions:set", 'versions:update-child-modules', '-DnewVersion=%s' % version], expected_code=0)
+def update_pom_files(version):
     base_dir = os.getcwd()
-    os.chdir('./signalfx-java-examples')
-    (stdout, _, code) = execute(["mvn", "versions:set", '-DnewVersion=%s' % version], expected_code=0)
+    logging.info('Updating POM files to version %s...', version)
+    cmd = ['mvn', 'versions:set', 'versions:update-child-modules',
+           '-DnewVersion=%s' % version]
+    (stdout, _, code) = execute(cmd, expected_code=0)
+    os.chdir('signalfx-java-examples')
+    (stdout, _, code) = execute(cmd, expected_code=0)
     os.chdir(base_dir)
 
 
-def update_connection_file():
-    files_to_change = ['signalfx-java/src/main/java/com/signalfx/connection/AbstractHttpReceiverConnection.java']
-    for file_name in files_to_change:
-        file_name = os.path.join(os.getcwd(), file_name)
-        with open(file_name, 'r') as file:
-            contents = file.read()
-        contents = re.sub(r'public static final String VERSION_NUMBER = "(.*?)"', r'public static final String VERSION_NUMBER = "%s"' % version, contents)
-        with open(file_name, 'w') as file:
-            file.write(contents)
-
-
-def main():
-    update_connection_file()
-    update_pom_files()
+def perform_file_replacements(version):
+    for file_name, repls in FILE_REPLACES.items():
+        logging.info('Updating %d version number location%s in %s...',
+                     len(repls), 's' if len(repls) != 1 else '', file_name)
+        for repl in repls:
+            logging.debug('%s -> %s', *repl)
+            file_name = os.path.join(os.getcwd(), file_name)
+            with open(file_name, 'r') as f:
+                contents = f.read()
+            contents = repl[0].sub(repl[1] % version, contents)
+            with open(file_name, 'w') as f:
+                f.write(contents)
 
 
 if __name__ == '__main__':
-    sys.exit(main())
+    if len(sys.argv) != 2:
+        sys.stderr.write('usage: %s <version>%n', sys.argv[0])
+        sys.exit(1)
+
+    version = sys.argv[1]
+    update_pom_files(version)
+    perform_file_replacements(version)
