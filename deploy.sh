1c29677c6253312e14a47ac131f979903873cb76
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2021-07-15 16:32:27 +0200

    Fix deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,7 @@
 set -e
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-ROOT_DIR="${SCRIPT_DIR}/../"
-cd ${ROOT_DIR}
+cd ${SCRIPT_DIR}
 
 print_usage() {
   echo "Usage: ./$(basename $0) [snapshot|release]"
