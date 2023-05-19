e32ea1387354bb309806758a852110d98edf9806
Lauri Tulmin <ltulmin@splunk.com>
2023-05-19 11:53:21 +0300

    delete deprecated method

--- signalfx-metrics/src/main/java/com/signalfx/metrics/SourceNameHelper.java
+++ signalfx-metrics/src/main/java/com/signalfx/metrics/SourceNameHelper.java
@@ -1,16 +1,7 @@
 package com.signalfx.metrics;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.net.InetAddress;
-import java.net.URL;
 import java.net.UnknownHostException;
-
-import java.nio.charset.StandardCharsets;
-
-import com.signalfx.metrics.aws.AWSInstanceInfo;
-import org.apache.http.entity.InputStreamEntity;
-import org.apache.http.util.EntityUtils;
 
 public final class SourceNameHelper {
     private SourceNameHelper() {
@@ -39,22 +30,5 @@ public final class SourceNameHelper {
         }
         throw new RuntimeException(
                 "Unable to find a default source name.  Please set one with usingDefaultSource()");
-    }
-
-    /**
-     * A helper to get the AWS instance ID.  Useful as an alternative default source name.
-     * @return AWS instance ID
-     * @throws IOException If there are any issues reading the local AWS endpoint.  For example,
-     *                     if the host is not on AWS.
-     * @deprecated Use {@link AWSInstanceInfo#get()} and set as a dimension on a metric or as an
-     *             unique dimension on a SignalFxReporter
-     */
-
-    @Deprecated
-    public static String getAwsInstanceId() throws IOException {
-        URL url = new URL("http://169.254.169.254/latest/meta-data/instance-id");
-        try (InputStream inputStream = url.openStream()) {
-            return EntityUtils.toString(new InputStreamEntity(inputStream), StandardCharsets.UTF_8);
-        }
     }
 }
